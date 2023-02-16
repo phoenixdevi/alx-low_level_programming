8c3ae7eeb9d4c10a1efd27194a7f752ec60a818e
Victor <phoenixtrader@yahoo.com>
2023-02-16 15:20:22 -0800

    corrected things

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
+/**
+ * main - it is the entry point 
+ */
 
 int main(void)
 {
-  puts("Programming is like building a multilingual puzzle");
-  return(0);
+ puts("\"Programming is like building a multilingual puzzle");
+ return (0);
 }  
