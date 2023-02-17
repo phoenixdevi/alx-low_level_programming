94ec517c5759013449ca0f05c21a8fdb20e72272
Victor <phoenixtrader@yahoo.com>
2023-02-16 16:02:18 -0800

    tackling betty

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
 /**
- * main - Entry 
+ * main - Entry
  *
  * Return: always 0
  */
 
 int main(void)
 {
-  printf("with proper grammar, but the outcome is a piece of art,\n");
-  return(0);
+printf("with proper grammar, but the outcome is a piece of art,\n");
+  return (0);
 }
