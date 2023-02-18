c0e8280e08e1faa12039744d7dce15627a2d5196
Victor <phoenixtrader@yahoo.com>
2023-02-17 17:18:19 -0800

    fixed by emacs

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -14,13 +14,14 @@ int main(void)
 	char low_case_alpha;
 	
 	low_case_alpha = 'a';
-		
-	while (low_case_alpha <= 'z')
+	
+	while
+	  (low_case_alpha <= 'z')
 		{
 			putchar(low_case_alpha);
-			++low_case_alpha;
-			  
+			++low_case_alpha;			  
 		}
+	
 	putchar('\n');
 	return (0);
 }
