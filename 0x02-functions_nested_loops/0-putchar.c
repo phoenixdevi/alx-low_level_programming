e2a47b8c1e623cf7dd0a541cd1e98d4349075f3c
Victor <phoenixtrader@yahoo.com>
2023-02-21 08:43:44 -0800

    putchar task

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -6,25 +6,25 @@ int main(void)
 {
 	int ch;
 
-      ch = '_';
-      putchar(ch);
-      ch = 'p';
-      putchar(ch);
-      ch = 'u';
-      putchar(ch);
-      ch = 't';
-      putchar(ch);
-      ch = 'c';
-      putchar(ch);
-      ch = 'h';
-      putchar(ch);
-      ch = 'a';
-      putchar(ch);
-      ch = 'r';
-      putchar(ch);
-      ch = '\n';
-      putchar(ch);
+	ch = '_';
+	putchar(ch);
+	ch = 'p';
+	putchar(ch);
+	ch = 'u';
+	putchar(ch);
+	ch = 't';
+	putchar(ch);
+	ch = 'c';
+	putchar(ch);
+	ch = 'h';
+	putchar(ch);
+	ch = 'a';
+	putchar(ch);
+	ch = 'r';
+	putchar(ch);
+	ch = '\n';
+	 putchar(ch);
 
-      return(0);
+	 return (0);
 }
 
