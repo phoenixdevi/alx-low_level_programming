82641690a462d02be5527a7c3ee8843bc752f2be
Victor <phoenixtrader@yahoo.com>
2023-02-21 10:37:10 -0800

    low case task

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -13,8 +13,12 @@ int main(void)
 {
 	for(char all_low_case_letters = 'a'; all_low_case_letters <= 'z'; all_low_case_letters++)
 
+	{
+
 		putchar(all_low_case_letters);
+
 	 	putchar('\n');
+	}
 
 	return(0);
  }
