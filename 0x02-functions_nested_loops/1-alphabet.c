fb07adfbd89e9c28a4dd987e6eac3cd5cec16083
Victor <phoenixtrader@yahoo.com>
2023-02-21 10:29:16 -0800

    low case task

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+
+/* *
+ * main - entry point
+ * Description: so it begins
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	for(char all_low_case_letters = 'a'; all_low_case_letters <= 'z'; all_low_case_letters++)
+
+		putchar(all_low_case_letters);
+	 	putchar('\n');
+
+	return(0);
+ }
+
