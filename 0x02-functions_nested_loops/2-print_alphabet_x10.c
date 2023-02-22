02b3939808deb49ad71a98aea74cc9108ec2307d
Victor <phoenixtrader@yahoo.com>
2023-02-22 06:36:57 -0800

    big things

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -8,9 +8,11 @@ void print_alphabet_x10(void)
 {
 	int line, ch;
 
-	for (line = 0; line <= 9; ++line){
+	for (line = 0; line <= 9; ++line)
+	{
 
-		for (ch = 'a'; ch <= 'z'; ++ch) putchar(ch);
+		for (ch = 'a'; ch <= 'z'; ++ch)
+		       	putchar(ch);
 
 		putchar('\n');
 	}
