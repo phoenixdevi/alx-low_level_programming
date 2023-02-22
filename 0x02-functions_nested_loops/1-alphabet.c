4cff3fd2b32ab9fa162ffd64a24f5016ced2e003
Victor <phoenixtrader@yahoo.com>
2023-02-21 16:33:03 -0800

    2 birds one stone

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,16 +1,22 @@
 #include <stdio.h>
-#include"main.h"
+#include "main.h"
 
 /**
- *   print_alphabet - utilizes on the _putchar function to print the alphabet a - z
+ *   print_alphabet - utilizes on the putchar function to print the alphabet a - 
+ *   main - Entry point 
+ *   Return: Always 0
 */
 
-void print_alphabet(void)
+void print_alphabet(void);
+int main(void)
 {
-		int ch;
+	int ch;
 
-			for (ch = 'a'; ch <= 'z'; ++ch)
-						putchar(ch);
-				putchar('\n');
+	for (ch = 'a'; ch <= 'z'; ++ch)
+
+		putchar(ch);
+
+	putchar('\n');
+
 }
 
---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include"main.h"
+
+/**
+ *  print_alphabet_x10 - function that will print the alphabet 10 times
+ *  main - Entry point 
+ *  Return: Always 0
+*/
+
+void print_alphabet_x10(void);
+int main(void)
+{
+	int line, ch;
+
+	for (line = 0; line <= 9; ++line)
+		{
+			for (ch = 'a'; ch <= 'z'; ++ch)
+			putchar(ch);
+			putchar('\n');
+		}
+
+	return(0);
+}
