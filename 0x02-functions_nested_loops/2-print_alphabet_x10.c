d67d15745cd298404b6b6b5de2d038e9b0755c47
Victor <phoenixtrader@yahoo.com>
2023-02-21 19:40:50 -0800

    ppp

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -10,8 +10,9 @@
 void print_alphabet_x10(void)
 {
 	int line, ch;
-	for (line = 0; line <= 9; ++line)
-			{		
+
+	for ( line = 0; line < = 9; ++line )
+			{
 				for (ch = 'a'; ch <= 'z'; ++ch)
 					putchar(ch);
 
