73fba940d546b6a5fc417ea7cf61e5c36d6eb8e8
Victor <phoenixtrader@yahoo.com>
2023-02-21 18:52:49 -0800

    fixef

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -7,14 +7,15 @@
  *  Return: Always 0
 */
 
-void print_alphabet_x10(void);
+void print_alphabet_x10(void)
 {
 	int line, ch;
 
 	for (line = 0; line <= 9; ++line)
 		{
+			
 			for (ch = 'a'; ch <= 'z'; ++ch)
-			putchar(ch);
+				putchar(ch);
 			putchar('\n');
 		}
 
