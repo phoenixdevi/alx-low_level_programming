7d4bca1e35c45c9392559feb877058ba9cd49512
Victor <phoenixtrader@yahoo.com>
2023-02-23 14:01:29 -0800

    just dey play

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,7 +1,8 @@
 #include "main.h"
 
 /**
-* main - tests function that prints if integer is positive or negative
+* main - tests function that prints 
+* if integer is positive or negative
 * Return: 0
 */
 
@@ -9,7 +10,7 @@ int main(void)
 {
         int i;
 
-        i = 9;
+        i = 98 % 2;
         positive_or_negative(i);
 
         return (0);
