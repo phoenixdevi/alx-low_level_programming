7661e158a05cfda8f4e7854c2fa0ed0eb006de77
Victor <phoenixtrader@yahoo.com>
2023-02-22 16:24:02 -0800

    fixed

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -9,6 +9,7 @@ void times_table(void)
 {
 	int num, mult, prod;
 	for (num = 0; num <= 9; num++)
+
 	{
 		_putchar('0');
 
@@ -16,7 +17,6 @@ void times_table(void)
 		{
 			_putchar(',');
 			_putchar(' ');
-			
 			prod = num * mult;
 			if (prod <= 9)
 				_putchar(' ');
