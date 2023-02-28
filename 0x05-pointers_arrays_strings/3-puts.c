1bdd74de7c18726a77ca795670fb51be73b104eb
Victor <phoenixtrader@yahoo.com>
2023-02-28 12:25:06 -0800

    day 2 of this

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,19 +1,14 @@
-#include "main.h"
+#include <stdio.h>
 
 /**
  * _puts - prints a string
  * @str: the string
  *
  * Return: the length of the string
- */
-void puts(char *str)
-{
-	int i = 0;
+*/
 
-	while (str[i] != '\0')
-	{
-		putchar(str[i]);
-		i++;
+void _puts(char *str)
+{
+	puts(str);
 }
-	putchar('\n');
-}
+
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,5 +1,5 @@
-#ifndef VOID_H_INCLUDED
-#define VOID_H_INCLUDED
+#ifndef MAIN_H_INCLUDED
+#define MAIN_H_INCLUDED
 
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
@@ -9,5 +9,5 @@ void print_rev(char *s);
 void rev_string(char *s);
 void puts2(char *str);
 
-#endif /* VOID_H_INCLUDED */
+#endif /* MAIN_H_INCLUDED */
 
